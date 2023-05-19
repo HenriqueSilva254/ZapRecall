76a69bd91f62eeb7e5dbfc618eb296d3a6ad0a13
HenriqueSilva <HenriqueSilva254@gmail.com>
2023-05-19 20:37:34 -0300

    ZapRecall v.final cm Bonus

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import Reset from './css/reset.jsx'
+import Reset from './css/reset.js'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
